ca7e406ad53b89d231f3b6094be92b4f812de882
Allen Brubaker <allenbrubaker@gmail.com>
2016-03-04 14:18:01 -0500

    Added angular 2 to project and ran using jspm module loader.

---
+++ src/Dashboard/wwwroot/app/app.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component, View } from 'angular2/core';
+
+//create a simple angular component
+@Component({
+    selector: 'test-app'
+})
+@View({
+    template: '<h4>Hello {{name}}</h4>'
+})
+export class TestApp {
+    name: string;
+    constructor() {
+        this.name = 'Angular2';
+        setTimeout(() => {
+            this.name = 'Angular2!!!'
+        }, 1500);
+    }
+}
+
